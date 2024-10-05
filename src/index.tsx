d58310102ea1202239d706d6dec41fc73e39dd20
Nawwar14 <nawwarkheder@gmail.com>
2024-10-05 22:23:12 +0300

    Start

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/MainPage/MainPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+type cardProperties={
+  Premium:boolean;
+  Price:number;
+  Img:string;
+  ApartsmentType:string;
+  Description:string;
+  NumberOfPlaces?:number;
+};
+export type {cardProperties};
+
+const mainPageCardInfo:cardProperties[] = [
+  {
+    Premium:true,
+    Price:120,
+    Img:'../markup/Img/apartment-01.jpg',
+    ApartsmentType:'Apartment',
+    Description:'Beautiful &amp; luxurious apartment at great location',
+    NumberOfPlaces:132
+  },
+  {
+    Premium:false,
+    Price:80,
+    Img:'../markup/Img/room.jpg',
+    ApartsmentType:'Room',
+    Description:'Wood and stone place',
+  },
+
+  {
+    Premium:false,
+    Price:132,
+    Img:'../markup/Img/apartment-02.jpg',
+    ApartsmentType:'Apartment',
+    Description:'Canal View Prinsengracht',
+  },
+
+  {
+    Premium:true,
+    Price:180,
+    Img:'../markup/Img/apartment-03.jpg',
+    ApartsmentType:'Apartment',
+    Description:'Nice, cozy, warm big bed apartment',
+  },
+
+  {
+    Premium:false,
+    Price:80,
+    Img:'../markup/Img/room.jpg',
+    ApartsmentType:'Room',
+    Description:'Wood and stone place',
+  }];
+export default {mainPageCardInfo} ;
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App CardProps={mainPageCardInfo}/>
   </React.StrictMode>
 );
