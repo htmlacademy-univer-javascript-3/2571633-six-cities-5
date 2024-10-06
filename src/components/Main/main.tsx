af2772ed4465458a516feb6d3a70d190b65118a3
Nawwar14 <147875072+Nawwar14@users.noreply.github.com>
2024-10-06 11:14:22 +0300

    update main remove unused type

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -4,7 +4,6 @@ import { LoginPage } from '../Login/LoginPage';
 import { Offer } from '../Offer/Offer';
 import { Favorite } from '../Favorites/Favorite';
 import { Error404 } from '../Error/Error404';
-import {mainPageCardInfo} from '../../index.tsx';
 import {cardProperties} from '../../index.tsx';
 import { UserContextProvider } from '../User';
 import { LoggedRoute } from '../LoggedRoute';
