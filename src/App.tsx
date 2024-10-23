4a8f60667c10f78b04147a6c22056ef6a76d5f35
Nawwar14 <147875072+Nawwar14@users.noreply.github.com>
2024-10-24 01:19:10 +0300

    T51V0

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import Favorite from './components/Favorites/Favorite';
+import LoginPage from './components/Login/LoginPage';
+import Offer from './components/Offer/Offer';
+
+type Offer = {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  type: string;
+  isPremium: boolean;
+  previewImage: string;
+  NumberOfPlaces: number;
+  latitude: number;
+  longitude:number;
+};
+
+type AppProps = {
+  offers: Offer[];
+};
+
+export default function App({ offers }: AppProps) {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage offers={offers} />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/offer/:id" element={<Offer />} />
+        <Route path="/favorites" element={<Favorite offers={offers} />} />
+      </Routes>
+    </Router>
+  );
+}
