89bd695bc1d35464bcd761ae55f6927e85e23a1e
hani.mohammad <hanimohammadhaidi@hotmail.com>
2024-12-21 12:45:34 +0300

    testcheck

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -82,7 +82,7 @@ function LoginPage(): JSX.Element {
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <Link className="locations__item-link" to={AppRoute.Main}>
+              <Link data-testid='location_item-link' className="locations__item-link" to={AppRoute.Main}>
                 <span>Amsterdam</span>
               </Link>
             </div>
